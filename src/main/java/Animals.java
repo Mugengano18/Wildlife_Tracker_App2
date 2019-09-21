06ff82474a1b8b66f3c6f83cf689ac0094d3e4dc
Mugengano18 <mugenganoa@gmail.com>
2019-09-21 13:37:00 +0200

    i added some codes

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,2 +1,26 @@
-public class Animals {
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+public abstract class Animals {
+    public int  id;
+    public String name;
+    public boolean danger;
+    public Timer timer;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean isEndangered() {
+        return danger;
+    }
+
+
+
+
+
 }
