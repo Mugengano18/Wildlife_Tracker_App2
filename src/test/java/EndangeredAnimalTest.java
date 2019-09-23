8f679ce3e3712929fbbd29569fad0c62634e8bfa
Mugengano18 <mugenganoa@gmail.com>
2019-09-23 08:14:04 +0200

    i added some codes and tests

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,5 +1,28 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EndangeredAnimalTest {
+    @Test
+    public void endangered_instatiateCorrectly(){
+        EndangeredAnimal testEndanger= new EndangeredAnimal("tiger","sick","adult");
+        assertEquals(true,testEndanger instanceof  EndangeredAnimal);
+    }
+
+    @Test
+    public void Endangerd_instantiateName_String(){
+        EndangeredAnimal testEndanger= new EndangeredAnimal("tiger","sick","adult");
+        assertEquals("tiger",testEndanger.getName());
+    }
+    @Test
+    public void Endangerd_instantiatehealth_String(){
+        EndangeredAnimal testEndanger= new EndangeredAnimal("tiger","sick","adult");
+        assertEquals("sick",testEndanger.getHealth());
+    }
+    @Test
+    public void Endangerd_instantiateAge_String(){
+        EndangeredAnimal testEndanger= new EndangeredAnimal("tiger","sick","adult");
+        assertEquals("adult",testEndanger.getAge());
+    }
 
 }
